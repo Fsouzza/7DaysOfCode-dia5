03c09b6639b3a1a7c112ec64f3d1b8e853c634ec
F. Souzza <filipesouzza.adm@gmail.com>
2022-04-05 22:09:22 -0300

    alteração para day 6

--- DaysOfCode_day5.js
+++ DaysOfCode_day5.js
@@ -1,10 +1,14 @@
-var categorias = ["Hortifrut", "Açougue", "Limpeza", "Bebidas", "Laticinios", "Padaria", "Enlatados"]
+var categorias = ["Hortifrut", "Açougue", "Limpeza", "Bebidas", "Laticinios", "Padariaa", "Enlatados"]
 var lista = []
 var produto = ""
 var quantidade = ""
 var categoria = ""
-var divCategorias = ""
 
+createSelectCategorias()
+
+function createSelectCategorias(){
+
+var divCategorias = ""
 divCategorias += "Categoria: <select name='categoriasSelect' id='categoriasSelect'>"
 
 for (var item in categorias) {
@@ -13,8 +17,10 @@ for (var item in categorias) {
 
 divCategorias += "</select>" 
 document.getElementById("divCategorias").innerHTML = divCategorias
+}
 
 function addItem() {
+
     produto = document.getElementById("item").value
     quantidade = document.getElementById("qtde").value
     categoria = document.getElementById("categoriasSelect").value
@@ -49,16 +55,87 @@ function finalizar() {
 
 
 limparConteudo()
+
     document.getElementById("conteudo").innerHTML = htmlCode
 
-}
-
-function limparConteudo() {
-    document.getElementById("conteudo").innerHTML = ""
 }
 
 
 function removeItem() {
     var remover = lista.pop();
     alert("voce acabou de deletar o ultimo produto inserido")
+}
+
+
+function excluirLista(){
+    var htmlCode = ""
+
+    htmlCode += "Item da Lista: <select name='itemSelect' id='itemSelect'>"
+
+    for (var item in lista) {
+        htmlCode += "<option value='" + lista[item].produto + "'>" + lista[item].produto + "</option>"
+    }
+
+htmlCode += "</select>"
+htmlCode += "<br><button class='delButton' onclick='excluirElemento()' >Excluir Item</button>"
+htmlCode += "<br><button class='normButton' onclick='home()' >Home</button>"
+
+finalizar()
+document.getElementById("conteudo").innerHTML = htmlCode
+
+}
+
+function excluirElemento() {
+    var item = document.getElementById("itemSelect").value
+
+        for(var i in lista) {
+            if (lista[i].produto.toLowerCase() == item.toLowerCase()) {
+                lista.splice(i, 1)
+            }
+        }
+
+excluirLista()
+
+}
+
+function home() {
+    limparConteudo()
+    var htmlCode = ""
+
+        htmlCode += "<h3>Adicione o produto e quantidade a Lista</h3>"
+        htmlCode += "Produto <br>  <input type='text' id='item'><br>"
+        htmlCode += "Quatidade <br> <input type='number' id='qtde' min='0' max='50'><br>"
+        htmlCode += "<div id='divCategorias'></div>"
+        htmlCode += "<br><button class='add' onclick='addItem()'>Adicionar</button><br>"
+        htmlCode += "<button class='remove' onclick='removeItem()'>Remover</button><br><button class='exclui' onclick='excluirLista()'>Excluir</button><br>"
+        htmlCode += "<button class='print' onclick='finalizar()''>Visualizar</button><br>"
+        htmlCode += "<div class='msg'>"
+        htmlCode += "<p id='msg'></p>"
+        htmlCode += "</div>"
+
+        document.getElementById("conteudo").innerHTML = htmlCode
+
+        createSelectCategorias()
+}
+
+function limparConteudo() {
+    document.getElementById("conteudo").innerHTML = ""
+}
+
+function limparDivCategorias() {
+    document.getElementById("divCategorias").innerHTML = ""
+}
+
+function addButtonsContent() {
+    var htmlCode = ""
+    htmlCode += "<br><button class='NormButton' onclick='home()' >Adicionar Novos</button><button class='NormButton' onclick='newLista()' >Nova Lista</button>"
+
+    document.getElementById("conteudo").innerHTML += htmlCode
+    console.log("Não aparece na tela essa informação")
+}
+
+
+function newLista() {
+    lista=[]
+    home()
 }
